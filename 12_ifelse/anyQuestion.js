4d003937a293072743c0479f5b42118e8fb4058a
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-03 20:25:13 +0300

    Update anyQuestion.js

--- 12_ifelse/anyQuestion.js
+++ 12_ifelse/anyQuestion.js
@@ -1,4 +1,15 @@
-var message = (login == 'Вася') ? 'Привет' :
-  (login == 'Директор') ? 'Здравствуйте' :
-  (login == '') ? 'Нет логина' :
-  '';
+var login = prompt ("введите логин","");
+if (login==null){
+alert ('вход отменён');
+}
+else if (login == "Admin") 
+{
+	var pass= prompt ("введите пароль","");
+
+	var check = (pass==null) ?
+	'вход отменён': (pass == "1111") ? "welcome, дружище" :"Я вас не узнаю";
+	
+}
+else {
+alert ("Я вас не знаю!");
+}
