5d3f4c4c4a8a84a1a8fc11a008583fda5a39015f
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-04 23:52:23 +0300

    Update and rename 17_function/pow.js to 02_17_04_function/pow.js

---
+++ 02_17_04_function/pow.js
@@ -0,0 +1,18 @@
+function pow(x, n) {
+    var result = x;
+
+    for (var i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+var x = prompt("x?", '');
+var n = prompt("n?", '');
+
+if (n <= 1) {
+    alert('введите корректную тепень ' + n);
+} else {
+    alert(pow(x, n));
+}
