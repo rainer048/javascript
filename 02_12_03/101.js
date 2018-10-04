e26988583d1e818827ac9f2392ccaeff7aca88c9
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-04 22:48:53 +0300

    Update and rename 12_ifelse/101.js to 02_12_03/101.js

---
+++ 02_12_03/101.js
@@ -0,0 +1,10 @@
+var num = prompt ('введите число','');
+
+if (+num > 0){
+  alert (1);
+}
+else if (+num < 0){
+  alert(-1);
+}
+else 
+  alert (0 );
--- 12_ifelse/101.js
+++
@@ -1,9 +0,0 @@
-var num = prompt ("введите число","");
-if (+num > 0){
-alert (1);
-}
-else if (+num < 0){
-alert(-1);
-}
-else 
-alert (0 );
