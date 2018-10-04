3c3e08a0f20fb738b4bf7246a110e63f4691b29c
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-04 22:40:45 +0300

    Update and rename 10_bitwise_operation/numberTest.js to 02_10_02_bitwise_operation/numberTest.js

---
+++ 02_10_02_bitwise_operation/numberTest.js
@@ -0,0 +1,7 @@
+function isInteger(num) {
+    return (num ^ 0) === num;
+    /*Для проверки равенства без преобразования типов используются операторы строгого равенства === (тройное равно) и !==.*/
+}
+alert(isInteger(3));
+alert(isInteger(43.245));
+alert(isInteger(-2335.22224));
