ac4824d5efba35bc8a45ef7bc8f8417e4d4f8770
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-05 00:00:26 +0300

    Update and rename 01_19_03_recursion/fibonachi.js to 02_19_03_recursion/fibonachi.js

---
+++ 02_19_03_recursion/fibonachi.js
@@ -0,0 +1,33 @@
+var many = +prompt("задайте ваше число фибоначчи?", '');
+var choise = +prompt("каким вариантом считать ваше число фибоначчи 1-екурсия, 2- цикл", '');
+
+switch (choise) {
+case 1:
+    {
+        function fib(n) {
+            return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+        }
+        alert(fib(many));
+    }
+    break;
+
+case 2:
+    {
+
+        function fib(n) {
+            var a = 1
+              , b = 1;
+            for (var i = 3; i <= n; i++) {
+                var c = a + b;
+                a = b;
+                b = c;
+            }
+            return b;
+        }
+        alert(fib(many));
+        break;
+    }
+
+default:
+    alert("надо было выбирать из предложенных вариантов");
+}
