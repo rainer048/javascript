ba60017a8efdd736c2fa69cd0e979bafa4e7e235
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-04 23:56:50 +0300

    Update factorial.js

--- 01_19_02_recursion/factorial.js
+++ 01_19_02_recursion/factorial.js
@@ -1,6 +1,6 @@
-var nn = +prompt("факториал какого числа хотите получить?",'');
+var nn = +prompt("факториал какого числа хотите получить?", '');
 function factorial(n) {
-  return (n != 1) ? n * factorial(n - 1) : 1;
+    return (n != 1) ? n * factorial(n - 1) : 1;
 }
 
-alert( factorial(nn) ); 
+alert(factorial(nn));
