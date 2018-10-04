1fd94eb9bb6cf7555078362bff2b0b889dbb353e
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-04 23:12:47 +0300

    Update and rename 12_ifelse/anyQuestion.js to 02_12_05_felse/anyQuestion.js

---
+++ 02_12_05_felse/anyQuestion.js
@@ -0,0 +1,11 @@
+var login = prompt('введите логин' , '');
+if (login == null) {
+    alert('вход отменён');
+} else if (login == 'Admin') {
+    var pass = prompt('введите пароль' , '');
+
+    var check = (pass == null) ? 'вход отменён' : (pass == '1111') ? 'welcome, дружище' : 'Я вас не узнаю';
+
+} else {
+    alert('Я вас не знаю!');
+}
