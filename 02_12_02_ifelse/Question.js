9e3e3a3e0167e96c7a2f15730fba3dd14639a01e
Dmitriy <gogolevdms@gmail.com>
2018-10-05 00:14:43 +0300

    Update Question.js
    Update style

--- 02_12_02_ifelse/Question.js
+++ 02_12_02_ifelse/Question.js
@@ -1,8 +1,7 @@
 var question = prompt ('Каково официальное название JavaScript, ну если вдруг вы догадываетесь' , '');
 
-  if (question == 'ЕСМАScript' || 'ECMAScript'){
-    alert ('Красава');
-  }
-  else {
-    alert('Ничего, это в сущности и не важно, но по секрету, это ECMAScript');
- }
+if (question == 'ЕСМАScript') {
+  alert ('Красава');
+} else {
+  alert('Ничего, это в сущности и не важно, но по секрету, это ECMAScript');
+}
