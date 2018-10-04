9ff7fcf7b567df4f2b5479d46f1365f5421a05d5
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-04 22:53:43 +0300

    Update ifElseQuestion.js

--- 12_ifelse/ifElseQuestion.js
+++ 12_ifelse/ifElseQuestion.js
@@ -1,7 +1,8 @@
-var question = prompt ("Каково «официальное» название JavaScript, ну если вдруг вы догадываетесь","");
-if (question == "ЕСМАScript" || "ECMAScript"){
-alert ("Красава");
-}
-else {
-alert("Ничего, это в сущности и не важно, но по секрету, это ECMAScript");
-}
+var question = prompt ('Каково официальное название JavaScript, ну если вдруг вы догадываетесь' , '');
+
+  if (question == 'ЕСМАScript' || 'ECMAScript'){
+    alert ('Красава');
+  }
+  else {
+    alert('Ничего, это в сущности и не важно, но по секрету, это ECMAScript');
+ }
