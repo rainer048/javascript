cd07e2065c83d153280845f58b64cc4a2e9a2612
Dmitriy <gogolevdms@gmail.com>
2018-10-05 00:16:27 +0300

    Update 101.js
    Update code

--- 02_12_03_ifelse/101.js
+++ 02_12_03_ifelse/101.js
@@ -1,10 +1,9 @@
 var num = prompt ('введите число','');
 
-if (+num > 0){
-  alert (1);
-}
-else if (+num < 0){
+if (+num > 0) {
+  alert(1);
+} else if (+num < 0) {
   alert(-1);
+} else 
+  alert(0);
 }
-else 
-  alert (0 );
