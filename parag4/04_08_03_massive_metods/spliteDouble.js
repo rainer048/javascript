52834c223b38b204784edda4e1d8004e8fecf3ab
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-08 21:25:14 +0300

    Create spliteDouble.js

---
+++ parag4/04_08_03_massive_metods/spliteDouble.js
@@ -0,0 +1,17 @@
+var obj = {
+    className: 'open menu menu'
+};
+
+function removeClass(obj, cls) {
+    var classes = obj.className.split(' ');
+
+    for (var i = 0; i < classes.length; i++) {
+        if (classes[i] == cls) {
+            classes.splice(i, 1); // удалить класс
+            i--; // (*)
+        }
+    }
+    obj.className = classes.join(' ');
+
+}
+
