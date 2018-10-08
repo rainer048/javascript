85e236ec3d67e4ca97f82e47eb59f9112e9eca26
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-08 18:01:58 +0300

    Create getMaxSubSum.js

---
+++ parag4/04_07_10_massive/getMaxSubSum.js
@@ -0,0 +1,16 @@
+function getMaxSubSum(arr) {
+    var maxSum = 0, partialSum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        partialSum += arr[i];
+        maxSum = Math.max(maxSum, partialSum);
+        if (partialSum < 0)
+            partialSum = 0;
+    }
+    return maxSum;
+}
+alert(getMaxSubSum([-1, 2, 3, -9]));
+alert(getMaxSubSum([-1, 2, 3, -9, 11]));
+alert(getMaxSubSum([-2, -1, 1, 2]));
+alert(getMaxSubSum([100, -9, 2, -3, 5]));
+alert(getMaxSubSum([1, 2, 3]));
+alert(getMaxSubSum([-1, -2, -3]));
