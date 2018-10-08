f90eb39bc2415aefe8af3b516d9d2eb6eb4f11e3
rainer048 <37981586+rainer048@users.noreply.github.com>
2018-10-09 01:56:38 +0300

    Codestyle
    JavaScript code has a column limit of 80 characters. (https://google.github.io/styleguide/jsguide.html#formatting-column-limit)

--- parag2/02_16_01_switch/switchToIfelse.js
+++ parag2/02_16_01_switch/switchToIfelse.js
@@ -1,7 +1,12 @@
 var browser = prompt('введите название вашего браузера', '')
 if (browser == 'IE') {
     alert('О, да у вас IE!');
-} else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+} else if (
+    browser == 'Chrome' || 
+    browser == 'Firefox' || 
+    browser == 'Safari' || 
+    browser == 'Opera'
+) {
     alert('Да, и эти браузеры мы поддерживаем');
 } else {
     alert('Мы надеемся, что и в вашем браузере все ок!');
