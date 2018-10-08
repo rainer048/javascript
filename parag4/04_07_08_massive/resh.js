0e6640e0a05642f48acd6851f0495b114380b21f
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-10-08 17:47:38 +0300

    Create resh.js

---
+++ parag4/04_07_08_massive/resh.js
@@ -0,0 +1,26 @@
+var arr = [];
+for (var i = 2; i < 100; i++) {
+    arr[i] = true
+}
+var p = 2;
+do {
+    for (i = 2 * p; i < 100; i += p) {
+        arr[i] = false;
+    }
+
+    // шаг 4
+    for (i = p + 1; i < 100; i++) {
+        if (arr[i]) break;
+    }
+
+    p = i;
+} while (p * p < 100); // шаг 5
+
+var sum = 0;
+for (i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+        sum += i;
+    }
+}
+
+alert( sum );
